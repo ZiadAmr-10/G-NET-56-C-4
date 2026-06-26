271935437c9288e82be80dac069d37ae8a7ea55e
ZiadAmr-10 <ziadddddamr@gmail.com>
2026-06-26 19:46:21 +0300

    Question01

--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -4,6 +4,30 @@
     {
         static void Main(string[] args)
         {
+            #region Question 01
+            DayOfWeek Day ;
+            bool Flag=false;
+            do
+            {
+                Console.WriteLine("Enter a day number (1-7) :");
+                bool isParse = int.TryParse(Console.ReadLine(), out int num );
+                if (isParse && num >= 1 && num <= 7)
+                { Day = (DayOfWeek)num;
+                    switch (Day) {
+                        case DayOfWeek.Saturday:
+                        case DayOfWeek.Friday:
+                            Console.WriteLine("Weekend");
+                            break;
+                        default:
+                            Console.WriteLine("WorkDay");
+                       break;
+                    }
+                    Flag = true;
+                }
+                else
+                    Console.WriteLine("Invalid Number Try again ");
+             } while (!Flag);
+            #endregion
         }
     }
 }
