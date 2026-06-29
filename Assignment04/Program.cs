1f5bf494840ff5a1cfb3f1a3fa318c4d1f8b6f2f
ZiadAmr-10 <ziadddddamr@gmail.com>
2026-06-29 19:11:09 +0300

    Question05

--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -79,46 +79,66 @@ namespace Assignment04
 
             #endregion
             #region Question04
-            double x, y;
-            bool isValid=false;
-            string operation;
-            do {
-                Console.WriteLine("Enter Number 1 : ");
-                bool isParsed= double.TryParse(Console.ReadLine(), out x);
-                Console.WriteLine("Enter Number 2 : ");
-                bool isparsed= double .TryParse(Console.ReadLine(),out y);
-                Console.WriteLine("Enter The Operation : +, -, *, / ");
-                operation = Console.ReadLine();
-                if (!isParsed || !isparsed)
+            //double x, y;
+            //bool isValid=false;
+            //string operation;
+            //do {
+            //    Console.WriteLine("Enter Number 1 : ");
+            //    bool isParsed= double.TryParse(Console.ReadLine(), out x);
+            //    Console.WriteLine("Enter Number 2 : ");
+            //    bool isparsed= double .TryParse(Console.ReadLine(),out y);
+            //    Console.WriteLine("Enter The Operation : +, -, *, / ");
+            //    operation = Console.ReadLine();
+            //    if (!isParsed || !isparsed)
+            //    {
+            //        Console.WriteLine("Invalid input\nTry Again :");
+            //    }
+            //    else if (operation == "/" && y == 0)
+            //    {
+            //        Console.WriteLine("Can't divided over Zero ! \n Try Again :");
+            //    }
+            //    else if(operation !="+"&& operation != "-"&&operation!="*"&& operation != "/")
+            //    {
+            //        Console.WriteLine("invalid operation !\nTry Again :  ");
+            //    }
+            //    else
+            //        isValid = true;
+            //} while (!isValid);
+            //switch (operation)
+            //{
+            //    case "+":
+            //        Console.WriteLine(Calculator.Add(x, y));   
+            //        break;
+            //    case "-":
+            //        Console.WriteLine(Calculator.Subtract(x, y));
+            //        break;
+            //    case "*":
+            //        Console.WriteLine(Calculator.Multiply(x, y));
+            //        break;
+            //    case "/":
+            //        Console.WriteLine(Calculator.Divide(x, y));
+            //        break;
+            //}
+            #endregion
+            #region Question05
+            double radius;
+            double area;
+            double circumference;
+            bool isValid = false;
+            do
+            {
+                Console.WriteLine("Enter Circle Radius : ");
+                bool isParsed = double.TryParse(Console.ReadLine(), out radius);
+                if (!isParsed || radius <= 0)
                 {
-                    Console.WriteLine("Invalid input\nTry Again :");
-                }
-                else if (operation == "/" && y == 0)
-                {
-                    Console.WriteLine("Can't divided over Zero ! \n Try Again :");
-                }
-                else if(operation !="+"&& operation != "-"&&operation!="*"&& operation != "/")
-                {
-                    Console.WriteLine("invalid operation !\nTry Again :  ");
+                    Console.WriteLine("Invalid Radius! Try Again.");
                 }
                 else
                     isValid = true;
             } while (!isValid);
-            switch (operation)
-            {
-                case "+":
-                    Console.WriteLine(Calculator.Add(x, y));   
-                    break;
-                case "-":
-                    Console.WriteLine(Calculator.Subtract(x, y));
-                    break;
-                case "*":
-                    Console.WriteLine(Calculator.Multiply(x, y));
-                    break;
-                case "/":
-                    Console.WriteLine(Calculator.Divide(x, y));
-                    break;
-            }
+            CircleCalculator.CalculateCircle(radius,out area,out circumference);
+            Console.WriteLine($"Area = {area}");
+            Console.WriteLine($"Circumference = {circumference}");
             #endregion
 
         }
